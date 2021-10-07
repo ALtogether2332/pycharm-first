a238b0d90796e7acb425928b321b6108b40abdeb
ALtogether2332 <alanmcloughlin12@gmail.com>
2021-10-07 08:43:27 +0100

    'HEREweGOAgain'

---
+++ main.py
@@ -0,0 +1,11 @@
+print('Hopefully I can upload this to Github')
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+x = np.arange(1,10,1)
+y = x**2
+
+plt.scatter(x,y)
+plt.show()
+
+
